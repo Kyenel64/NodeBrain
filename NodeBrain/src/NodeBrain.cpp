2e0d3240f3f5080d5acbdd3ec1adb7e553909b74
kyenelson3@gmail.com <kyenelson3@gmail.com>
2024-01-27 21:57:44 +0900

    glfw setup

--- NodeBrain/src/NodeBrain.cpp
+++ NodeBrain/src/NodeBrain.cpp
@@ -3,10 +3,53 @@
 
 #include "NodeBrain.h"
 #include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int TestWindow()
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwSetErrorCallback(error_callback);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
 
 int main()
 {
-	glm::vec3 vec = { 0.5f, 0.8f, 1.0f };
-	std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
-	return 0;
+    glm::vec3 vec = { 0.5f, 0.8f, 1.0f };
+    std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
+    TestWindow();
+    return 0;
 }
