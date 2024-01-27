7f8ccc620a6c94c1e4d4144269af6d0f36bbc9f9
Kye Nelson <kyenelson3@gmail.com>
2024-01-27 21:13:35 +0900

    Initial project setup + setup for submodules

---
+++ NodeBrain/src/NodeBrain.cpp
@@ -0,0 +1,12 @@
+﻿// NodeBrain.cpp : Defines the entry point for the application.
+//
+
+#include "NodeBrain.h"
+#include <glm/glm.hpp>
+
+int main()
+{
+	glm::vec3 vec = { 0.5f, 0.8f, 1.0f };
+	std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
+	return 0;
+}
