b1dc08e9a4d2b69de1b359a500d5c33ed63dcbd8
Kye Nelson <kyenelson3@gmail.com>
2024-06-20 18:45:06 +0900

    Removed timer test for now

--- Test/src/Core/Test_Timer.cc
+++ Test/src/Core/Test_Timer.cc
@@ -3,8 +3,5 @@
 
 TEST(Timer, GetElapsedMicroseconds) 
 {
-    NodeBrain::Timer timer;
-    timer.EndTimer();
-    EXPECT_EQ(timer.GetElapsedMicroseconds(), timer.GetElapsedMilliseconds() / 1000.0f);
-    EXPECT_EQ(timer.GetElapsedMicroseconds(), timer.GetElapsedSeconds() / 1000000);
+
 }
