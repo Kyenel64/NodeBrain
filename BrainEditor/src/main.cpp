261bdb2c3d1b781a6970283ed8ba50bda1b294c6
Kye Nelson <kyenelson3@gmail.com>
2024-08-23 17:20:30 +0900

    Fixed main file

--- BrainEditor/src/main.cpp
+++ BrainEditor/src/main.cpp
@@ -9,39 +9,19 @@ int main()
 {
 	NB_PROFILE_BEGIN("Profile", "Profile.log");
 
-	auto* window = new NodeBrain::Window("NodeBrain", 1280, 720);
-	auto* renderContext = new NodeBrain::VulkanRenderContext(*window);
-	auto* rendererAPI = new NodeBrain::VulkanRendererAPI(*renderContext);
-	auto* gui = new NodeBrain::VulkanImGuiLayer(*renderContext);
-	auto* renderer = new NodeBrain::Renderer(*rendererAPI);
-#ifdef NB_TEST_EDITOR
-	NodeBrain::Layer* testEditor = new NodeBrain::TestEditor(*renderer);
-	auto* app = new NodeBrain::App("NodeBrain", *window, *renderer, gui);
-	app->PushLayer(*testEditor);
-#else
-	NodeBrain::Layer* brainEditor = new NodeBrain::BrainEditor(*renderer);
-	auto* app = new NodeBrain::App("NodeBrain", *window, *renderer, gui);
-	app->PushLayer(*brainEditor);
-#endif
+	NodeBrain::Window window("NodeBrain", 1280, 720);
+	NodeBrain::VulkanRenderContext renderContext(window);
+	NodeBrain::VulkanRendererAPI rendererAPI(renderContext);
+	NodeBrain::VulkanImGuiLayer gui(renderContext);
+	NodeBrain::Renderer renderer(rendererAPI);
 
+	NodeBrain::App app("NodeBrain", window, renderer, &gui);
+	NodeBrain::TestEditor testEditor(renderer);
+	app.PushLayer(testEditor);
 
-	app->Run();
-
-
-	delete app;
-#ifdef NB_TEST_EDITOR
-	delete testEditor;
-#else
-	delete brainEditor;
-#endif
-	delete renderer;
-	delete gui;
-	delete rendererAPI;
-	delete renderContext;
-	delete window;
+	app.Run();
 
 	NB_PROFILE_END();
-
 
 	return 0;
 }
